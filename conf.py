b2cf6091049d2efcecc6b91d124ada02fd9aef4f
Oshane Bailey <b4.oshany@gmail.com>
2019-06-08 11:19:15 +0000

    set master doc.

--- conf.py
+++ conf.py
@@ -59,6 +59,8 @@ try:
 except ImportError:
     html_theme = 'default'
 
+master_doc = 'index'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
